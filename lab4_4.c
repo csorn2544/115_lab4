1f5d6d4b7ae73750314148aad76d0c197e4b721f
csorn2544 <aue_chanisor@hotmail.com>
2021-03-01 19:50:19 +0700

    Acronym

--- lab4_4.c
+++ lab4_4.c
@@ -3,21 +3,19 @@
 #include<ctype.h>
 int main()
 {
-    int i,temp;
-    char str[1000];
-    scanf("%s" ,str);
-    temp=strlen(str);
-    while(i<temp)
+    int i=0,j=0,k=0;
+    char str[1000],s[11][4] = {"i","of","the","on","at","for","with","a","an","in","and"},*temp;
+    gets(str);
+    temp=strtok(str," ");
+    while(temp!=NULL)
     {
-        if(i==0)
-        {
-            printf("%c",toupper(str[i]));
-        }
-        if(str[i]==' '&&str[i+1]!=' ')
-        {
-            printf("%c",toupper(str[i+1]));
-        }
-        i++;
+        for(i=0;i<11;i++)
+        if(strcmp(temp,s[i]))
+            j++;
+        if(j==11||k==0)
+         printf("%c",toupper(temp[0]));
+         temp=strtok(NULL," ");
+         j=0;
+         k++;
     }
 }
-
